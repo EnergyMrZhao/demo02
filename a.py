63ea03939ac61815a199ebcda461749ec25e97f1
EnergyMrZhao <75509768+EnergyMrZhao@users.noreply.github.com>
2023-06-05 15:51:40 +0800

    Update a.py

--- a.py
+++ a.py
@@ -1,2 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def sendMsg(codeSrc):
+    data = {
+        "token": "8906471ad908426db7f4e17d229bf46a",
+        "title": "通行码",
+        "content": codeSrc,
+    }
+    requests.post(url="http://www.pushplus.plus/send/", data=data)
+
+
 if __name__ == "__main__":
-  print("44")
+  sendMsg("886")
